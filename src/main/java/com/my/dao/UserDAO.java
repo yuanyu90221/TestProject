2caf666ec885c32346e2188a2c410ca447d09dbd
yuanyu90221 <yuanyu90221@gmail.com>
2016-09-21 12:14:37 +0800

    修改註解

--- src/main/java/com/my/dao/UserDAO.java
+++ src/main/java/com/my/dao/UserDAO.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 import com.my.model.User;
 
+/**
+ * @author YuanyuLiang
+ *
+ * @description userDao用來處理user資料的物件
+ */
 public interface UserDAO {
 	
 	public User findByUser(String strAccount, String strPassword);
